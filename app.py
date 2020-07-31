d6398b6bcde298353a3f3bb2fdaf904d4d0ed293
Vaibhav Dixit <vaibhavdixit1000@gmail.com>
2020-07-31 19:01:11 +0530

    Modified app.py file and packages

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -14,7 +16,7 @@ app = Flask(__name__)
 app.secret_key = 'vaibhav'
 api = Api(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] =os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_AUTH_URL_RULE'] = '/login'
 # config JWT to expire within half an hour
